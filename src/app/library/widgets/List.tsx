bcdeaa9a527e1de18489f00498af94fbaf5d417f
Michael Schwarz <schwarzm90@gmail.com>
2023-03-20 21:40:24 +0100

    remove unused component

--- src/app/library/widgets/List.tsx
+++
@@ -1,9 +0,0 @@
-export default function List({ array }: { array: string[] }) {
-  return (
-    <ul className="">
-      {array.map((item: string) =>
-        <li>item</li>
-      )}
-    </ul>
-  )
-}
